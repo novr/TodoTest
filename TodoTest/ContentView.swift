d4e07ba3d36bcb85cff1c4a5310d92ea07b2ee3c
n_komiya <n_komiya@yumemi.co.jp>
2020-06-18 11:30:02 +0900

    add: xcodeproj

---
+++ TodoTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoTest
+//
+//  Created by 古宮 伸久 on 2020/06/18.
+//  Copyright © 2020 YUMEMI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
